0e6cb4aae50eb84aca4296c4dd6fcdc1f9704695
Fredrik Halvarsson <Fredrik.Halvarsson@live.se>
2023-10-31 16:17:27 +0100

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb8OOPGenericCollections
+{
+    internal class Employee
+    {
+        public Employee(int id, string name, string gender, int salary)
+        {
+            ID = id;
+            Name = name;
+            Gender = gender;
+            Salary = salary;
+        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine("-----------------------Employee info-----------------------");
+            Console.WriteLine($"ID: {ID}\nName: {Name}\nGender: {Gender}\nSalary: {Salary}");
+            Console.WriteLine("-----------------------------------------------------------");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Labb8OOPGenericCollections
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Employee e1 = new Employee(100, "Fredrik", "Male", 14000);
+            Employee e2 = new Employee(101, "Therese", "Female", 35000);
+            Employee e3 = new Employee(102, "Kalle","Male",45000);
+            Employee e4 = new Employee(103, "Erik", "Male", 14000);
+            Employee e5 = new Employee(104, "Carina", "Female", 40000);
+
+            Stack<Employee> empStack = new Stack<Employee>();
+            empStack.Push(e1);
+            empStack.Push(e2);
+            empStack.Push(e3);
+            empStack.Push(e4);
+            empStack.Push(e5);
+
+            Console.WriteLine("***********************************************************");
+            foreach (var item in empStack)
+            {
+                item.Display();
+                Console.WriteLine(empStack.Count() + " Items left in stack");
+            }
+            Console.WriteLine("*************************** Pop ***************************");
+
+            do
+            {
+                var poppedEmp = empStack.Pop();
+                poppedEmp.Display();
+                Console.WriteLine(empStack.Count() + " Items left in stack");
+            } while (empStack.Count() > 0);
+            empStack.Push(e1);
+            empStack.Push(e2);
+            empStack.Push(e3);
+            empStack.Push(e4);
+            empStack.Push(e5);
+
+            Console.WriteLine("*************************** Peek **************************");
+            var peekEmp = empStack.Peek();
+            peekEmp.Display();
+            Console.WriteLine(empStack.Count() + " Items left in stack");
+            peekEmp = empStack.Peek();
+            peekEmp.Display();
+            Console.WriteLine(empStack.Count() + " Items left in stack");
+
+            Console.WriteLine("************************* contains ************************");
+            if (empStack.Contains(e3))
+            {
+                Console.WriteLine("Employee 3 is in stack");
+            }
+            else
+            {
+                Console.WriteLine("Employee 3 is not in stack");
+            }
+
+            Console.WriteLine("************************** Part 2 *************************");
+            Console.WriteLine("*************************** List **************************");
+            List<Employee> empList = new List<Employee>() { e1,e2,e3,e4,e5};
+            if (empList.Contains(e3))
+            {
+                Console.WriteLine("Employee 3 object exists in list");
+            }
+            else
+            {
+                Console.WriteLine("Employee 3 object does not exists in list");
+            }
+
+            Console.WriteLine("*************************** Find **************************");
+            var firstMaleEmp = empList.Find(e => e.Gender == "Male");
+            firstMaleEmp.Display();
+
+            Console.WriteLine("************************* Find All ************************");
+            List<Employee> maleEmpList = empList.FindAll(e => e.Gender == "Male");
+            foreach(var item in maleEmpList)
+            {
+                item.Display();
+            }
+        }
+    }
+}
